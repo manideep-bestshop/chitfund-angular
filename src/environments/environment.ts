20fe99f6f19b9a54d67ba79160688a2bde8ede6b
Manideep Nampally <157095258+manideep-bestshop@users.noreply.github.com>
2026-03-09 12:00:18 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7254' // Later, change this to your live server URL when you deploy
+  apiUrl: 'https://chitfundapi20260304152452.azurewebsites.net/api' // Later, change this to your live server URL when you deploy
 };
